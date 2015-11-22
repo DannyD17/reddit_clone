0317ae502f6d644c3451092c344f1c23699056c6
Danny Debevec <daniel.debevec@gmail.com>
2015-11-22 11:46:29 -0700

    Added current user   *Added helper method for current use   *Set session id when creating new user   *Updated view to show current session

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,7 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
+        session[:id] = @user.id
         format.html { redirect_to root_path, notice: "Thank you for signing up #{@user.first_name.capitalize}" }
         format.json { render :show, status: :created, location: @user }
       else
