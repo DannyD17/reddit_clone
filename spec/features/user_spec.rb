14fd00865bbb89b410fc85baef31cb68061faa41
Danny Debevec <daniel.debevec@gmail.com>
2015-11-22 11:55:48 -0700

    Added login spec

--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 feature 'User authentication' do
-  scenario 'It should allow users to sign up' do
+  scenario 'It should allow users to signup' do
     user = FactoryGirl.create(:user)
 
     visit '/'
@@ -19,6 +19,24 @@ feature 'User authentication' do
     click_button 'Signup'
 
     expect(page).to have_content("Thank you for signing up #{user.first_name.capitalize}")
+    expect(page).to have_content("Signed in as #{user.email}")
+
+  end
+  scenario 'It should allow users to login' do
+    user = FactoryGirl.create(:user)
+
+    visit '/'
+
+    expect(page).to have_link('Login')
+
+    click_link 'Login'
+
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+
+    click_button 'Login'
+
+    expect(page).to have_content("Welcome back #{user.first_name.capitalize}")
     expect(page).to have_content("Signed in as #{user.email}")
 
   end
