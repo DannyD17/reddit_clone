41d295e5d3fb80188e7e432d50e0973ecfa93d44
Danny Debevec <daniel.debevec@gmail.com>
2015-11-22 11:20:53 -0700

    Added user authentication tests

---
+++ spec/features/user_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+feature 'User authentication' do
+  scenario 'It should allow users to sign up' do
+    user = FactoryGirl.create(:user)
+
+    visit '/'
+
+    expect(page).to have_link('Signup')
+
+    click_link 'Signup'
+
+    fill_in 'First Name', with: user.first_name
+    fill_in 'Last Name', with: user.last_name
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+    fill_in 'Password Confirmation', with: user.password
+
+    expect(page).to have_content("Thank you for signing up #{user.first_name.capitalize}")
+    expect(page).to have_content("You are signed in as #{user.email}")
+
+  end
+end
